003ea48cc98dd0947bb7b8e801f839ce16fec4ef
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-15 23:37:12 +0330

    fixes

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
