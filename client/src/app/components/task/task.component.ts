209f6da8edde25c98711228f2f599623d967a89e
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-16 19:54:52 +0330

    added task form

--- client/src/app/components/task/task.component.ts
+++ client/src/app/components/task/task.component.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -9,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+  @Output() addTask: EventEmitter<string> = new EventEmitter<string>();
 
+  readonly #fb = inject(FormBuilder);
+
+  taskGroup = this.#fb.group({
+    titleCtrl: [null, [Validators.required]]
+  });
+
+  // getter
+  get TitleCtrl(): FormControl {
+    return this.taskGroup.get('titleCtrl') as FormControl;
+  }
+
+  onSubmit(): void {
+    if (this.taskGroup.valid) {
+      this.addTask.emit(this.TitleCtrl.value);
+    }
+  }
 }
