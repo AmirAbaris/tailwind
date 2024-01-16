9503e1c282bd56d7ebfc8425e1d4360b59f0bda9
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-16 23:09:31 +0330

    tring to use counter

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { TaskService } from '../../services/task.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,15 @@ import { TaskComponent } from '../task/task.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly #taskService = inject(TaskService);
 
+  taskCount: number = 0;
+
+  countAllTasks(): void {
+    this.#taskService.tasks$.pipe(takeUntilDestroyed()).subscribe({
+      next: (tasks) => {
+        this.taskCount = tasks.length;
+      }
+    });
+  }
 }
