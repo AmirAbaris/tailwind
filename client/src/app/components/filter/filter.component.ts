bc454f71bb7dc1a0b9e64c23ecb8fbe5e1c6f4e0
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-16 19:20:45 +0330

    clean up

--- client/src/app/components/filter/filter.component.ts
+++ client/src/app/components/filter/filter.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, TaskComponent],
+  imports: [TaskComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
