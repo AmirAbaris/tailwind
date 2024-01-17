3c14a44547115229fa9ed8d0bf2b28ad3ffe37a0
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-17 18:15:10 +0330

    added cypress test, still incomplete

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,6 +1,17 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+describe('add a task process', () => {
+  beforeEach(() => {
+    // visit home url
+    cy.visit('');
+  });
+
+  it('should add a task', () => {
+    // click the button with text 'Add task'
+    cy.get('button').contains('Add task').click();
+
+    // add input text
+    cy.get('input[formControlName="titleCtrl"]').type('lets test this cypress!');
+
+    // click on submit btn
+    cy.get('.test button').contains('Add').should('exist').click();
+  });
+});
