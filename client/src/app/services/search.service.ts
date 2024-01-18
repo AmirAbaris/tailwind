f41e5f60080e21284e545e45ba21fe87c153109c
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-18 18:24:49 +0330

    added search service

---
+++ client/src/app/services/search.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private searchTermSubject = new BehaviorSubject<string | null>(null);
+
+  setSearchTerm(term: string): void {
+    this.searchTermSubject.next(term);
+  }
+
+  getSearchTerm(): BehaviorSubject<string | null> {
+    return this.searchTermSubject;
+  }
+}
