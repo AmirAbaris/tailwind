26e75f377098a04c44d374a2831929f335eec83c
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-23 16:16:06 +0330

    fixes

--- client/src/app/components/to-do-task/to-do-task.component.ts
+++ client/src/app/components/to-do-task/to-do-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { FilterPipe } from '../../pipes/filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -19,6 +19,17 @@ import { Task } from '../../models/task.model';
   styleUrl: './to-do-task.component.css'
 })
 export class ToDoTaskComponent {
-@Input() searchTerm: string = '';
-@Input() todoTasks: Task[] = [];
+  @Input() searchTerm: string = '';
+  @Input() todoTasks: Task[] = [];
+
+  @Output() deleteTaskEvent = new EventEmitter<string>();
+  @Output() completeTaskEvent = new EventEmitter<string>();
+
+  onDeleteTask(taskId: string): void {
+    this.deleteTaskEvent.emit(taskId);
+  }
+
+  onCompleteTask(taskId: string): void {
+    this.completeTaskEvent.emit(taskId);
+  }
 }
