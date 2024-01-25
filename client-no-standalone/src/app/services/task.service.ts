fd9110f50ca9314a081873f10c1052b912e84c3f
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-25 22:38:52 +0330

    updated the service to simulate the observable return

--- client-no-standalone/src/app/services/task.service.ts
+++ client-no-standalone/src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, delay, map, of, tap } from 'rxjs';
 import { Task } from '../models/task.model';
 import Chance from 'chance';
 
@@ -10,76 +10,91 @@ export class TaskService {
   private tasksSource = new BehaviorSubject<Task[]>([]);
   tasks$ = this.tasksSource.asObservable();
 
-  add(taskTitle: string): void {
-    // added chance library to give a random input in id
-    // * not the best practice to add a id tbh, but it'll do the job for this project
-    let chance = new Chance();
+  add(taskTitle: string): Observable<null> {
+    // simulate HTTP request delay
+    return of(null).pipe(
+      delay(500),
+      // update tasks locally
+      tap(() => {
+        const chance = new Chance();
 
-    const currentTask: Task = {
-      id: chance.string({ length: 8 }),
-      title: taskTitle,
-      completed: false
-    }
-
-    // update the observable with new input value
-    const updatedTasks = [...this.tasksSource.value, currentTask];
-
-    // update ob value
-    this.tasksSource.next(updatedTasks);
+        const newTask: Task = {
+          id: chance.string({ length: 8 }),
+          title: taskTitle,
+          completed: false
+        };
+        this.tasksSource.next([...this.tasksSource.value, newTask]);
+      })
+    );
   }
 
-  delete(taskId: string): void {
-    // find the target tasks index to delete
-    const targetTask = this.tasksSource.value.findIndex(task => task.id === taskId);
+  delete(taskId: string): Observable<null> {
+    return of(null).pipe(
+      delay(500),
+      tap(() => {
+        // find the target tasks index to delete
+        const targetTask = this.tasksSource.value.findIndex(task => task.id === taskId);
 
-    // if item not found
-    // * -1 is used for not found items in a index!
-    if (targetTask !== -1) {
-      // create a copy of the tasks array
-      const updatedTasks = [...this.tasksSource.value];
+        // if item not found
+        // * -1 is used for not found items in a index!
+        if (targetTask !== -1) {
+          // create a copy of the tasks array
+          const updatedTasks = [...this.tasksSource.value];
 
-      // remove the target task
-      updatedTasks.splice(targetTask, 1);
+          // remove the target task
+          updatedTasks.splice(targetTask, 1);
 
-      // update the observable with the new array of tasks
-      this.tasksSource.next(updatedTasks);
-    }
+          // update the observable with the new array of tasks
+          this.tasksSource.next(updatedTasks);
+        }
+      })
+    );
   }
 
-  complete(taskId: string): void {
-    // find the target task
-    const targetTaskIndex = this.tasksSource.value.findIndex(task => task.id === taskId);
+  complete(taskId: string): Observable<null> {
+    return of(null).pipe(
+      delay(500),
+      tap(() => {
+        // find the target task
+        const targetTaskIndex = this.tasksSource.value.findIndex(task => task.id === taskId);
 
-    // check if the task was found
-    if (targetTaskIndex !== -1) {
-      // create a copy of the tasks array
-      const updatedTasks = [...this.tasksSource.value];
+        // check if the task was found
+        if (targetTaskIndex !== -1) {
+          // create a copy of the tasks array
+          const updatedTasks = [...this.tasksSource.value];
 
-      // complete the task
-      updatedTasks[targetTaskIndex].completed = true;
+          // complete the task
+          updatedTasks[targetTaskIndex].completed = true;
 
-      // update the observable
-      this.tasksSource.next(updatedTasks);
+          // update the observable
+          this.tasksSource.next(updatedTasks);
 
-      // log the obj
-      console.log(this.tasksSource.value);
-    }
+          // log the obj
+          console.log(this.tasksSource.value);
+        }
+      })
+    );
   }
 
-  inComplete(taskId: string): void {
-    // find the target task
-    const targetTaskIndex = this.tasksSource.value.findIndex(task => task.id == taskId);
+  inComplete(taskId: string): Observable<null> {
+    return of(null).pipe(
+      delay(500),
+      tap(() => {
+        // find the target task
+        const targetTaskIndex = this.tasksSource.value.findIndex(task => task.id == taskId);
 
-    // check if the task was found
-    if (targetTaskIndex !== -1) {
-      // create a copy of the task array
-      const updatedTasks = [...this.tasksSource.value];
+        // check if the task was found
+        if (targetTaskIndex !== -1) {
+          // create a copy of the task array
+          const updatedTasks = [...this.tasksSource.value];
 
-      // incomplete the task
-      updatedTasks[targetTaskIndex].completed = false;
+          // incomplete the task
+          updatedTasks[targetTaskIndex].completed = false;
 
-      // update the obsesrvable
-      this.tasksSource.next(updatedTasks);
-    }
+          // update the obsesrvable
+          this.tasksSource.next(updatedTasks);
+        }
+      })
+    );
   }
 }
