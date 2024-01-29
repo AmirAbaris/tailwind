5b2881c6f41a73d79fb10d2ac44afd1ae2e805e2
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-29 19:07:46 +0330

    added new service to simulate the back end

---
+++ client-no-standalone/src/app/services/local-storage.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  //#region behavior subject
+  private taskSource = new BehaviorSubject<Task[]>([]);
+  tasks$ = this.taskSource.asObservable();
+  //#endregion
+
+  addTask(task: Task): void {
+    const tasks: Task[] = [...this.taskSource.value, task];
+
+    this.taskSource.next(tasks);
+  }
+
+  deleteTask(taskId: string): void {
+    const tasks: Task[] = this.taskSource.value.filter(task => task.id !== taskId);
+
+    this.taskSource.next(tasks);
+  }
+
+  completeTask(taskId: string): void {
+    const tasks: Task[] = this.taskSource.value.map(task => {
+      if (task.id === taskId) {
+        task.completed = true;
+      }
+
+      return task;
+    });
+
+    this.taskSource.next(tasks);
+  }
+
+  inCompleteTask(taskId: string): void {
+    const tasks: Task[] = this.taskSource.value.map(task => {
+      if (task.id === taskId) {
+        task.completed = false;
+      }
+
+      return task;
+    });
+
+    this.taskSource.next(tasks);
+  }
+}
