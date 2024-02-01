670cc10ccf6a9ec0a2ea40de5bce6897b0ea6b3d
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-02 02:32:46 +0330

    clean up

--- client-no-standalone/src/app/app-routing.module.ts
+++ client-no-standalone/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskManagementMainComponent } from './components/task-management/task-management-main/task-management-main.component';
 import { I18nComponent } from './components/i18n/i18n.component';
 
 const routes: Routes = [
